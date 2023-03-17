bee606ae196b493410a8e3313dcaf037902a9765
Robert <robert.mabry86@gmail.com>
2023-03-16 19:39:21 -0500

    starting over with most js files

--- api/promise.js
+++ api/promise.js
@@ -7,7 +7,6 @@ const getWords = (uid) => new Promise((resolve, reject) => {
     method: 'GET',
     headers: {
       'Content-Type': 'application/json',
-      'Cache-Control': 'no-cache'
     },
   })
     .then((response) => response.json())
@@ -15,59 +14,57 @@ const getWords = (uid) => new Promise((resolve, reject) => {
     .catch(reject);
 });
 
-const makeWords = (payload) => new Promise((resolve, reject) => {
-  fetch(`${url}/vocab.json`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(payload),
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      resolve(data);
-    })
-    .catch(reject);
-});
+// const makeWords = (payload) => new Promise((resolve, reject) => {
+//   fetch(`${url}/vocab.json`, {
+//     method: 'POST',
+//     headers: {
+//       'Content-Type': 'application/json',
+//     },
+//     body: JSON.stringify(payload),
+//   })
+//     .then((response) => response.json())
+//     .then((data) => {
+//       resolve(data);
+//     })
+//     .catch(reject);
+// });
 
-const updateWords = (payload) => new Promise((resolve, reject) => {
-  fetch(`${url}/vocab/${payload.firebaseKey}.json`, {
-    method: 'PATCH',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(payload),
-  })
-    .then((response) => response.json())
-    .then((data) => resolve(data))
-    .catch(reject);
-});
+// const updateWords = (payload) => new Promise((resolve, reject) => {
+//   fetch(`${url}/vocab/${payload.firebaseKey}.json`, {
+//     method: 'PATCH',
+//     headers: {
+//       'Content-Type': 'application/json',
+//     },
+//     body: JSON.stringify(payload),
+//   })
+//     .then((response) => response.json())
+//     .then((data) => resolve(data))
+//     .catch(reject);
+// });
 
-const deleteWord = (firebaseKey) => new Promise((resolve, reject) => {
-  fetch(`${url}/vocab/${firebaseKey}.json?`, {
-    method: 'DELETE',
-    headers: {
-      'Content-Type': 'application/json',
-      'Cache-Control': 'no-cache'
-    }
-  })
-    .then((response) => response.json())
-    .then((data) => resolve(data))
-    .catch(reject);
-});
+// const deleteWord = (firebaseKey) => new Promise((resolve, reject) => {
+//   fetch(`${url}/vocab/${firebaseKey}.json?`, {
+//     method: 'DELETE',
+//     headers: {
+//       'Content-Type': 'application/json',
+//       'Cache-Control': 'no-cache'
+//     }
+//   })
+//     .then((response) => response.json())
+//     .then((data) => resolve(data))
+//     .catch(reject);
+// });
 
-const oneThing = (firebaseKey) => new Promise((resolve, reject) => {
-  fetch(`${url}/vocab/${firebaseKey}.json`, {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  })
-    .then((response) => response.json())
-    .then((data) => resolve(data))
-    .catch(reject);
-});
+// const oneThing = (firebaseKey) => new Promise((resolve, reject) => {
+//   fetch(`${url}/vocab/${firebaseKey}.json`, {
+//     method: 'GET',
+//     headers: {
+//       'Content-Type': 'application/json',
+//     },
+//   })
+//     .then((response) => response.json())
+//     .then((data) => resolve(data))
+//     .catch(reject);
+// });
 
-export {
-  getWords, makeWords, updateWords, deleteWord, oneThing
-};
+export default getWords;
--- utils/clearDom.js
+++ utils/clearDom.js
@@ -1,5 +1,5 @@
 const clearDom = () => {
-  document.querySelector('#words').innerHTML = '';
+  document.querySelector('#wordBox').innerHTML = '';
   document.querySelector('#add-button').innerHTML = '';
   document.querySelector('#form-container').innerHTML = '';
   document.querySelector('#view').innerHTML = '';
