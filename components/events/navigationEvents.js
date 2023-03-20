39e0e3c15b6e690210a2cb0c84a60b140022663d
Robert <robert.mabry86@gmail.com>
2023-03-20 18:47:16 -0500

    Update navigationEvents.js
    added filters

--- components/events/navigationEvents.js
+++ components/events/navigationEvents.js
@@ -1,16 +1,26 @@
 import { signOut } from '../../utils/auth';
 import showWordForm from '../forms/showWordForm';
+import { jsFilter, htmlFilter,cssFilter, getWords } from '../../api/promise';
 
 const navigationEvents = () => {
-  document.querySelector('#navigation').addEventListener('click', (e) => {
-    if (e.target.id.includes('#logout-button')) {
-      signOut();
-    }
-    if (e.target.id.includes('#create-word')) {
-      showWordForm();
-    }
-    // if (e.target.id.includes(#filter-word)) {}
-  });
+  document.querySelector('#logout-button').addEventListener('click', signOut);
+
+  document.querySelector('#show-all').addEventListener('click', () => {
+    getWords(user.uid).then(showWordForm);
+  })
+
+  //Filters
+  document.querySelector('#jsFilter').addEventListener('click', () => {
+    jsFilter(user.uid).then(showWordForm);
+  })
+
+  document.querySelector('#htmlFilter').addEventListener('click', () => {
+    htmlFilter(user.uid).then(showWordForm);
+  })
+
+  document.querySelector('#cssFilter').addEventListener('click', () => {
+    cssFilter(user.uid).then(showWordForm);
+  })
 };
 
 export default navigationEvents;
