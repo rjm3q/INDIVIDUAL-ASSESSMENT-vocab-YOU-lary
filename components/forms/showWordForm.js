8d8f12acd3905b1bfa24acae11f498472c571e88
Robert <robert.mabry86@gmail.com>
2023-03-20 19:41:37 -0500

    added form functions

--- components/forms/showWordForm.js
+++ components/forms/showWordForm.js
@@ -1,32 +1,32 @@
 import clearDom from '../../utils/clearDom';
 import renderToDOM from '../../utils/renderToDom';
 
-const showWordForm = (obj = {}) => {
+const addWordForm = (obj = {}) => {
   clearDom();
   const domString = `
-  <form id="${obj.firebaseKey ? `update-word--${obj.firebaseKey}` : 'submit-word'}" class="mb-4">
-    <div class="form-group" id="form-group">
-      <label for="title">Word</label>
-      <input type="text" class="form-control" id="title" aria-describedby="bookTitle" placeholder="Enter Word Here" value="${obj.Name || ''}" required>
-    </div>
-    <div class="form-group" id="form-group">
-      <label for="definition">Definition</label>
-      <input type="text" class="form-control" id="definition" aria-describedby="bookTitle" placeholder="Enter Definition" value="${obj.Definition || ''}" required>
-    </div>
-    <div id="form-group">
-      <label for="word-type">Select a Category</label>
-      <select class="form-control" id="word-type" required>
-        <option value="">Select an option</option>
-        <option value="JavaScript">JavaScript</option>
-        <option value="HTML">HTML</option>
-        <option value="CSS">CSS</option>
-      </select>
-    </div>
-    <button type="submit" class="btn btn-primary" id="submit-button">Submit Word
-    </button>
-    <a id="add-a-category" href="#">Add a Category</a>
-  </form>`;
+  <form id="${obj.firebaseKey ? `update-vocab--${obj.firebaseKey}` : 'submit-vocab'}" class="mb-4">
+      <div class="form-group">
+        <label for="title">Title</label>
+        <input type="text" class="form-control" id="title" aria-describedby="bookTitle" placeholder="Enter A Vocabulary" value="${obj.Name || ''}" required>
+      </div>
+      <div class="form-group">
+        <label for="description">Definition</label>
+        <textarea class="form-control" placeholder="Description" id="description" style="height: 100px">${obj.Definition || ''}</textarea>
+      </div>
+      <div class="form-group" id="select-language">
+      <label for="category">Language</label>
+        <select class="form-control" placeholder="Select Category" id="language" name="vocabCategory" value="${obj.Type || ''}" required>
+        <option value="">Select a Language</option>
+          <option value="HTML" ${obj.Type === 'HTML' ? 'selected' : ''}>HTML</option>
+          <option value="CSS" ${obj.Type === 'CSS' ? 'selected' : ''}>CSS</option>
+          <option value="JavaScript" ${obj.Type === 'JavaScript' ? 'selected' : ''}>JavaScript</option>
+        </select>
+      </div>
+      <br>
+      <button type="submit" class="btn btn-primary">Submit
+      </button>
+    </form>`;
   renderToDOM('#form-container', domString);
 };
 
-export default showWordForm;
+export default addWordForm;
