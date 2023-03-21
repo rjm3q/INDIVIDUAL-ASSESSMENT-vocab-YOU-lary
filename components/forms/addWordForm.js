83deefd6d0d0065b6e8ffb92dc574a347fa1a255
Robert <robert.mabry86@gmail.com>
2023-03-20 19:40:09 -0500

    added form functions

--- components/forms/addWordForm.js
+++ components/forms/addWordForm.js
@@ -4,16 +4,28 @@ import renderToDOM from '../../utils/renderToDom';
 const addWordForm = (obj = {}) => {
   clearDom();
   const domString = `
-  <form id="${obj.firebaseKey ? `update-word--${obj.firebaseKey}` : 'submit-word'}" class="mb-4">
+  <form id="${obj.firebaseKey ? `update-vocab--${obj.firebaseKey}` : 'submit-vocab'}" class="mb-4">
       <div class="form-group">
-        <label for="word">Word</label>
-        <input type="text" class="form-control" id="word" aria-describedby="wordTitle" placeholder="Enter Vocab Word" value="${obj.Name || ''}" required>
+        <label for="title">Title</label>
+        <input type="text" class="form-control" id="title" aria-describedby="bookTitle" placeholder="Enter A Vocabulary" value="${obj.Name || ''}" required>
       </div>
       <div class="form-group">
-        <label for="define">Definition</label>
-        <textarea class="form-control" placeholder="Vocab Definition" id="define" style="height: 100px">${obj.Definition || ''}</textarea>
+        <label for="description">Definition</label>
+        <textarea class="form-control" placeholder="Description" id="description" style="height: 100px">${obj.Definition || ''}</textarea>
       </div>
-  `;
+      <div class="form-group" id="select-language">
+      <label for="category">Language</label>
+        <select class="form-control" placeholder="Select Category" id="language" name="vocabCategory" value="${obj.Type || ''}" required>
+        <option value="">Select a Language</option>
+          <option value="HTML" ${obj.Type === 'HTML' ? 'selected' : ''}>HTML</option>
+          <option value="CSS" ${obj.Type === 'CSS' ? 'selected' : ''}>CSS</option>
+          <option value="JavaScript" ${obj.Type === 'JavaScript' ? 'selected' : ''}>JavaScript</option>
+        </select>
+      </div>
+      <br>
+      <button type="submit" class="btn btn-primary">Submit
+      </button>
+    </form>`;
   renderToDOM('#form-container', domString);
 };
 
